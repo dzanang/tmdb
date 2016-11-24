90ef0b5a778b68e592090f3a0a95360559187c2d
Dzanan <dzanang@gmail.com>
2016-11-24 16:48:05 +0100

    Making UI changes

--- Rubicon/app.js
+++ Rubicon/app.js
@@ -27,6 +27,12 @@ app.config(function ($stateProvider, $urlRouterProvider) {
                     var url = 'https://api.themoviedb.org/3/movie/' + $stateParams.id + '?api_key=5a06c55fb8ca078014f8be4126cb9a73&language=en-US';
                     return $http.get(url)
                         .then(function (res) { return res.data; });
+                },
+
+                movcast: function ($http, $stateParams) {
+                    var url = 'https://api.themoviedb.org/3/movie/' + $stateParams.id + '/credits?api_key=5a06c55fb8ca078014f8be4126cb9a73';
+                    return $http.get(url)
+                    .then(function (res) { return res.data; });
                 }
             }
         })
@@ -50,12 +56,12 @@ app.config(function ($stateProvider, $urlRouterProvider) {
           })
 
         .state('searchMov', {
-            url: '/movie/:search',
+            url: '/movie/:query',
             templateUrl: 'views/search-movies.html',
             controller: 'MoviesController',
             resolve: {
                 details: function ($http, $stateParams) {
-                    var url = 'https://api.themoviedb.org/3/search/movie?api_key=5a06c55fb8ca078014f8be4126cb9a73&language=en-US&query=' + $stateParams.search;
+                    var url = 'https://api.themoviedb.org/3/search/movie?api_key=5a06c55fb8ca078014f8be4126cb9a73&language=en-US&query=' + $stateParams.query;
                     return $http.get(url)
                         .then(function (res) { return res.data; });
                 }
@@ -140,20 +146,44 @@ app.controller("TvshowsController", function ($scope, $http, $rootScope, dataSer
     }
 });
 
-app.controller('MovDetController', ['$scope', 'details', function ($scope, details) {
+app.controller('MovDetController', ['$scope', 'details', 'movcast', function ($scope, details, movcast) {
 
     $scope.title = details.original_title;
-    console.log($scope.title)
-
     $scope.overview = details.overview;
-
     $scope.poster = "http://image.tmdb.org/t/p/w370/" + details.poster_path;
-    console.log($scope.poster);
-
     $scope.rating = details.vote_average;
     $scope.release = details.release_date;
     $scope.genres = details.genres;
     $scope.genre = $scope.genres.map(function (obj) { return obj.name; }).join(', ');
+    $scope.status = details.status;
+    if ($scope.status == "In Production") {
+        $scope.movStyle = {
+            "color": "blue",
+            "font-weight": "bold"
+        }
+    }
+
+    if ($scope.status == "Planned") {
+        $scope.movStyle = {
+            "color": "yellow",
+            "font-weight": "bold"
+        }
+    }
+
+    if ($scope.status == "Released") {
+        $scope.movStyle = {
+            "color": "green",
+            "font-weight": "bold"
+        }
+    }
+
+    $scope.actors = movcast.cast;
+    $scope.crew = movcast.crew;
+    for (i = 0; i < $scope.crew.length; i++) {
+        if ($scope.crew[i].job == "Director") {
+            $scope.director = $scope.crew[i].name;
+        }
+    }
 }]);
 
 
@@ -169,20 +199,19 @@ app.controller('TvDetController', ['$scope', 'tvdetails', 'tvcast', function ($s
     $scope.seasons = tvdetails.number_of_seasons;
     $scope.status = tvdetails.status;
     if ($scope.status == "Ended") {
-        $scope.myStyle = {
+        $scope.tvStyle = {
             "color": "red",
             "font-weight": "bold"
         }
     }
 
     if ($scope.status == "Returning Series") {
-        $scope.myStyle = {
+        $scope.tvStyle = {
             "color": "green",
             "font-weight": "bold"
         }
     }
 
     $scope.actors = tvcast.cast;
-    console.log($scope.actors);
-
+    $scope.crew = tvcast.crew;
 }]);
